6bc779da102ace105629dce17debfa4f9b50b3e6
Youngchan <michael_cho77@naver.com>
2020-03-27 06:01:41 +0900

    UserModel, Admin
    @admin.register(models.User)
     == admin.site.register(models.User, CustomUserAdmin)
    
    list display
    - admin의 list페이지 변화에 따라 어떤 필드를 보여줄지 결정할 수 있음  (admin계정에서 패널에 대한 display를 보여줌 )
    
    list_filter
    	을 통해서 필드의 유무에관한 필터링이 가능.
    
    admin 확장을 위해
    from django.contrib.auth.admin import UserAdmin
    을 상속함
    단 이렇게하면 사용자가 작성한 필드들은 모두사라짐.
    이를 추가하기위해 fieldset(필드들의 그룹) 사용

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.User)
+# 같은 의미 :  admin.site.register(models.Userm CustomUserAdmin)
+class CustomUserAdmin(UserAdmin):
+
+    """ Custom User Admin """
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
