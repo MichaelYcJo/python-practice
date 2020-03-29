a794157475702146ee48ed5ba09a6cb8d051aadd
Youngchan <michael_cho77@naver.com>
2020-03-30 01:49:49 +0900

    Reservations(Model, Admin)
    +
    def in_progress(self)
    def is_finished(self)

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
+from . import models
 
-""" Review Admin Definition """
 
-list_display = ("__str__", "rating_average")
+@admin.register(models.Review)
+class ReviewAdmin(admin.ModelAdmin):
+
+    """ Review Admin Definition """
+
+    list_display = ("__str__", "rating_average")
