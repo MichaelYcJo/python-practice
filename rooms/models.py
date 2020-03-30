27050628e35b2f51273809db99a4dc32f9e91876
Youngchan <michael_cho77@naver.com>
2020-03-31 00:19:19 +0900

    Seed_Rooms(1)
    .

--- rooms/models.py
+++ rooms/models.py
@@ -102,6 +102,8 @@ class Room(core_models.TimeStampedModel):
         # reviews의 models.py 의 room의 related_name="reviews"를 가지고 있으므로 room은 reviews를 가지고 있음
         all_reviews = self.reviews.all()
         all_ratings = 0
-        for review in all_reviews:
-            all_ratings += review.rating_average()
-        return all_ratings / len(all_reviews)
+        if len(all_reviews) > 0:
+            for review in all_reviews:
+                all_ratings += review.rating_average()
+            return all_ratings / len(all_reviews)
+        return 0
