713e03156ff45ad8742688c1f4421afd46c85673
Youngchan <michael_cho77@naver.com>
2020-03-31 17:23:52 +0900

    Class Based View(Rooms)
    class based view를 정리한 사이트
      https://ccbv.co.uk/
    
    List View
    해당페이지가 objecnt의 목록을 대변해줌.
    
    class Based Views에서는
      def get_context_data를 통해 function view에서 활용한 context를 사용할 수 있다.
    
    근데 listView에서는 context_object_name 으로 활용가능

--- rooms/views.py
+++ rooms/views.py
@@ -1,19 +1,21 @@
-from math import ceil
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage
+from django.utils import timezone
+from django.views.generic import ListView
 from . import models
 
 
-def all_rooms(request):
-    # paginator을 사용할경우 default를 1로잡아주기때문에 "page", 1을 따로 선언할 필요 없음
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    # Paginator(object_list, per_page(페이지당 개수))
-    paginator = Paginator(room_list, 10, orphans=5)
-    # print(vars(page.paginator))
+class HomeView(ListView):
 
-    try:
-        rooms = paginator.page(int(page))
-        return render(request, "rooms/home.html", {"page": rooms})
-    except EmptyPage:
-        return redirect("/")
+    """ HomeView Definition """
+
+    # 어떤 모델을 list화 할것인가
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
