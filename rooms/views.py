38e052175c0e4a08cb95b7e1e6585972d3a30973
Youngchan <michael_cho77@naver.com>
2020-03-31 17:05:27 +0900

    Pagenation(Using own logic with Paginator)
    Paginator
    https://docs.djangoproject.com/en/3.0/ref/paginator/
    
        paginator.get_page() => page가 범위를 초과하면 가장 마지막 페이지를 보여줌
        paginator.page() => 범위초과시 오류

--- rooms/views.py
+++ rooms/views.py
@@ -1,24 +1,19 @@
 from math import ceil
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from . import models
 
 
 def all_rooms(request):
+    # paginator을 사용할경우 default를 1로잡아주기때문에 "page", 1을 따로 선언할 필요 없음
     page = request.GET.get("page", 1)
-    page = int(page or 1)
-    page_size = 10
-    limit = page_size * page
-    offset = limit - page_size
-    all_rooms = models.Room.objects.all()[offset:limit]
-    page_count = ceil(models.Room.objects.count() / page_size)
-    return render(
-        request,
-        "rooms/home.html",
-        {
-            "potato": all_rooms,
-            "page": page,
-            "page_count": page_count,
-            # 템플릿에선 range함수를 사용할수 없으므로 만들어서 보내줌
-            "page_range": range(1, page_count),
-        },
-    )
+    room_list = models.Room.objects.all()
+    # Paginator(object_list, per_page(페이지당 개수))
+    paginator = Paginator(room_list, 10, orphans=5)
+    # print(vars(page.paginator))
+
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/home.html", {"page": rooms})
+    except EmptyPage:
+        return redirect("/")
