8a1ebc2a5781c68295821eecd136dd9e1f1c76c3
Youngchan <michael_cho77@naver.com>
2020-03-31 15:58:54 +0900

    Pagenation(Using own logic)
    Request 관련 DOC
    https://docs.djangoproject.com/en/3.0/ref/request-response/

--- rooms/views.py
+++ rooms/views.py
@@ -1,9 +1,24 @@
+from math import ceil
 from django.shortcuts import render
 from . import models
 
-# Create your views here.
-
 
 def all_rooms(request):
-    all_rooms = models.Room.objects.all()
-    return render(request, "rooms/home.html", context={"potato": all_rooms})
+    page = request.GET.get("page", 1)
+    page = int(page or 1)
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    all_rooms = models.Room.objects.all()[offset:limit]
+    page_count = ceil(models.Room.objects.count() / page_size)
+    return render(
+        request,
+        "rooms/home.html",
+        {
+            "potato": all_rooms,
+            "page": page,
+            "page_count": page_count,
+            # 템플릿에선 range함수를 사용할수 없으므로 만들어서 보내줌
+            "page_range": range(1, page_count),
+        },
+    )
