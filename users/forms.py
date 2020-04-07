1abc554eb446a9e6abc8e976848be211f95bc2f1
Youngchan <michael_cho77@naver.com>
2020-04-07 14:25:00 +0900

    Social Login, footer, Login, Signup
    widgets, attr을 통한 form 수정

--- users/forms.py
+++ users/forms.py
@@ -4,13 +4,16 @@ from . import models
 
 class LoginForm(forms.Form):
 
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder": "Email"}))
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Password"})
+    )
 
     def clean(self):
         # cleaned_data를 email, pwd 변수에 넣음
         email = self.cleaned_data.get("email")
         password = self.cleaned_data.get("password")
+
         try:
             # 해당 email과 같은 username이 있는 모델을 찾아 유효성검사를함
             user = models.User.objects.get(email=email)
@@ -28,8 +31,19 @@ class SignUpForm(forms.ModelForm):
         model = models.User
         fields = ("first_name", "last_name", "email")
 
-    password = forms.CharField(widget=forms.PasswordInput)
-    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+        # fields에 대한 일괄적인 수정
+        widgets = {
+            "first_name": forms.TextInput(attrs={"placeholder": "First Name"}),
+            "last_name": forms.TextInput(attrs={"placeholder": "Last Name"}),
+            "email": forms.EmailInput(attrs={"placeholder": "Email Name"}),
+        }
+
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Password"})
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Confirm Password"})
+    )
 
     def clean_password1(self):
         password = self.cleaned_data.get("password")
