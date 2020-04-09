6912ce540c314161479eb47128d2937be29bed03
Youngchan <michael_cho77@naver.com>
2020-04-09 23:20:41 +0900

    Calendar Image
    https://docs.python.org/3/library/calendar.html
    
    monthdays2calendar() -> 해당 년, 월의 weekList를 return해준다.

---
+++ cal.py
@@ -0,0 +1,36 @@
+import calendar
+
+
+class Calendar(calendar.Calendar):
+    def __init__(self, year, month):
+        super().__init__(firstweekday=6)
+        self.year = year
+        self.month = month
+        self.day_names = ("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat")
+        self.months = (
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+        )
+
+    def get_days(self):
+        # monthdays2calendar() -> 해당 년, 월의 weekList를 return해준다.
+        weeks = self.monthdays2calendar(self.year, self.month)
+        days = []
+        for week in weeks:
+            for day, _ in week:
+                days.append(day)
+        return days
+
+    def get_month(self):
+        # calendar의 1월은 리스트의 0이기 때문에 -1 해주어야함
+        return self.months[self.month - 1]
