a145565e6374de78e58cdd9a9198775206413c3a
Youngchan <michael_cho77@naver.com>
2020-04-12 01:56:46 +0900

    Create Superuser in AWS, (usermanager)
    가지고 있던 usermanger는 nomal한 usermanager지만
    유저는 다른 매니저를 가지고있어서 오류 발생

--- core/managers.py
+++ core/managers.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import UserManager
 
 
 class CustomModelManager(models.Manager):
@@ -7,3 +8,7 @@ class CustomModelManager(models.Manager):
             return self.get(**kwargs)
         except self.model.DoesNotExist:
             return None
+
+# superuser을 만드는 create_super()은 UserManager에 내장되어있는 메소드임
+class CustomUserManager(CustomModelManager, UserManager):
+    pass
