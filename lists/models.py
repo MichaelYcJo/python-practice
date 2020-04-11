0f1c123c3310f861ba390d22deed64ce19b0bab7
Youngchan <michael_cho77@naver.com>
2020-04-11 10:03:22 +0900

    Fav_Rooms(2)
    https://docs.djangoproject.com/en/3.0/topics/db/examples/many_to_many/
    
        list/view.py의 toggle메소드의 add, remove등의 메소드가 궁긍하다면 many to many 를 검색하면 뜸
        이제 각각룸에 저장된 list들을 볼수 있는 view를 구현해야 함
    
        userProfile과 django users를 사용하는 방법중 한가지는 user와 profile 사이에 OnetoOne을 이용하는 것

--- lists/models.py
+++ lists/models.py
@@ -9,8 +9,8 @@ class List(core_models.TimeStampedModel):
     name = models.CharField(max_length=80)
     # models.OneToOneField()를 사용하면 한 유저는 하나의 리스트만 가질 수 있다.
     # 이는 현재 fav_list가 get_or_create()등을 사용해서 유저가 2개이상의 리스트를 가지면 멀티플에러를 발생시킬 수 있기때문
-    user = models.ForeignKey(
-        "users.User", related_name="lists", on_delete=models.CASCADE
+    user = models.OneToOneField(
+        "users.User", related_name="list", on_delete=models.CASCADE
     )
     rooms = models.ManyToManyField("rooms.Room", related_name="lists", blank=True)
 
--- lists/views.py
+++ lists/views.py
@@ -1,15 +1,33 @@
 from django.shortcuts import redirect, reverse
+from django.views.generic import TemplateView
 from rooms import models as room_models
 from . import models
 
 
-def save_room(request, room_pk):
+def toggle_room(request, room_pk):
+    action = request.GET.get("action", None)
     room = room_models.Room.objects.get_or_none(pk=room_pk)
-    if room is not None:
+    if room is not None and action is not None:
         # 첫번째는 사용하려는 모델의 인스턴스, 두번째는 boolean flag이다.
         # get_or_create는 한개만 찾기때문에 유저의 리스트가 복수개라면 에러가 발생한다.(multiple objects have been returned)
         the_list, _ = models.List.objects.get_or_create(
             user=request.user, name="My Favourites Houses"
         )
-        the_list.rooms.add(room)
+        if action == "add":
+            the_list.rooms.add(room)
+        elif action == "remove":
+            the_list.rooms.remove(room)
     return redirect(reverse("rooms:detail", kwargs={"pk": room_pk}))
+
+
+class SeeFavsView(TemplateView):
+
+    template_name = "lists/list_detail.html"
+
+
+"""
+def see_favs(request):
+    user = request.user
+    user.lists.all()
+"""
+
