f5c6f743d28ac5dba24f63e06820635f189279cc
Youngchan <michael_cho77@naver.com>
2020-08-13 01:18:21 +0900

    Renderer
    .

--- instagram/urls.py
+++ instagram/urls.py
@@ -7,7 +7,8 @@ router = DefaultRouter()
 router.register('post', views.PostViewSet)
 
 urlpatterns = [
+    path('mypost/<int:pk>/', views.PostDetailAPIView.as_view()),
     path('', include(router.urls)),
-    path('public/', views.public_post_list),
+    #path('public/', views.public_post_list),
     
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.decorators import api_view, action
+from rest_framework.generics import RetrieveAPIView
 from rest_framework.response import Response
+from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from .serializers import PostSerializer
@@ -56,3 +58,15 @@ class PostViewSet(ModelViewSet):
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
     '''
+
+class PostDetailAPIView(RetrieveAPIView):
+    queryset = Post.objects.all()
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'instagram/post_detail.html'
+
+    def get(self, request, *args, **kwargs):
+        post = self.get_object()
+        return Response({
+            'post': post,
+            #'post': PostSerializer(post).data
+        })
