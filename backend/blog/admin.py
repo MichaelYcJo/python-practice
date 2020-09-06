5df7e19603c8f8e25e3236f5d05f9f3b5cdad589
Youngchan <michael_cho77@naver.com>
2020-09-06 21:20:39 +0900

    SummerNoteSetting, Model
    https://github.com/summernote/django-summernote

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import BlogPost
 
-# Register your models here.
+class BlogPostAdmin(SummernoteModelAdmin): 
+    summernote_fields = ('content')
+
+admin.site.register(BlogPost, BlogPostAdmin)
--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from datetime import datetime
+from django.template.defaultfilters import slugify
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Categories(models.TextChoices):
+        WORLD = 'world'
+        ENVIRONMENT = 'environment'
+        TECHNOLOGY = 'technology'
+        DESIGN = 'design'
+        CULTURE = 'culture'
+        BUSINESS = 'business'
+        POLITICS = 'politics'
+        OPINION = 'opinion'
+        SCIENCE = 'science'
+        HEALTH = 'health'
+        STYLE = 'style'
+        TRAVEL = 'travel'
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField()
+    category = models.CharField(max_length=50, choices=Categories.choices, default=Categories.WORLD)
+    thumbnail = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    excerpt = models.CharField(max_length=150, blank=False)  # 한줄 요약과같은 모델필드
+    month = models.CharField(max_length=3)
+    day = models.CharField(max_length=2)
+    content = models.TextField()
+    featured = models.BooleanField(default=False)
+    date_created = models.DateTimeField(default=datetime.now, blank=True)
+
