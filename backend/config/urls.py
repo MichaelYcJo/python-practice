cd7dd09ae72e0dff1e1a1d386ef0f79dfdca1cbd
Youngchan <michael_cho77@naver.com>
2020-09-07 02:09:52 +0900

    Detail, Category
    .

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -13,4 +13,4 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 #리액트 단에서 반응하기 위함 
-#urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
+urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
--- frontend/src/components/BlogDetail.js
+++ frontend/src/components/BlogDetail.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
+const BlogDetail = (props) => {
+    const [blog, setBlog] = useState({});
 
-const blogDetail = () => (
+    useEffect(() => {
+        const slug = props.match.params.id;
 
-    <div>
-        blogDetail
-    </div>
-);
+        const fetchData = async () => {
+            try {
+                const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/blog/${slug}`);
+                setBlog(res.data);
+            }
+            catch (err) {
 
+            }
+        }
 
-export default blogDetail;
+        fetchData();
+    }, [props.match.params.id]);
+
+    const capitalizeFirstLetter = (word) => {
+        if (word)
+            return word.charAt(0).toUpperCase() + word.slice(1);
+        else
+            return '';
+    };
+
+    const createBlog = () => {
+        return { __html: blog.content };
+    };
+
+    return (
+        <div className='container mt-3'>
+            <h1 className='display-2'>{blog.title}</h1>
+            <h2 className='text-muted mt-3'>Category: {capitalizeFirstLetter(blog.category)}</h2>
+            <h4 className=''>{blog.month} {blog.day}</h4>
+            <h4><img src={blog.thumbnail} /></h4>
+            <div className='mt-5 mb-5' dangerouslySetInnerHTML={createBlog()} />
+            <hr />
+            <p className="lead mb-5"><Link to='/blog' className="font-weight-bold">Back to Blogs</Link></p>
+        </div>
+    );
+}
+
+export default BlogDetail;
