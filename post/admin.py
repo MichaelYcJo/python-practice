c6cda66aac4df7bc8f69b453e0149292aed601d6
Youngchan <michael_cho77@naver.com>
2021-03-12 23:45:10 +0900

    RM - Space

--- post/admin.py
+++ post/admin.py
@@ -5,61 +5,39 @@ from .models import Post, Like, Bookmark, Tag
 
 class PostForm(forms.ModelForm):
     content = forms.CharField(widget=forms.Textarea)
-    
+
     class Meta:
         model = Post
         fields = '__all__'
 
+
 class LikeInline(admin.TabularInline):
     model = Like
 
-          
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['id', 'author', 'nickname', 'content', 'created_at']
     list_display_links = ['author', 'nickname', 'content']
     form = PostForm
     inlines = [LikeInline]
-    
+
     def nickname(request, post):
         return post.author.profile.nickname
+
 
 @admin.register(Like)
 class LikeAdmin(admin.ModelAdmin):
     list_display = ['id', 'post', 'user', 'created_at']
     list_display_links = ['post', 'user']
-    
+
+
 @admin.register(Bookmark)
 class BookmarkAdmin(admin.ModelAdmin):
     list_display = ['id', 'post', 'user', 'created_at']
-    list_display_links = ['post', 'user']    
-    
+    list_display_links = ['post', 'user']
+
+
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ['name']
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-
-
-
-
-
-
-
-    
-    
-    
-    
-    
-    
-    
