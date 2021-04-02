19eec28a3dc6811ba6cd88e33be4e502a50c20da
Youngchan <michael_cho77@naver.com>
2021-04-03 01:13:04 +0900

    ADD - UpdateView

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,11 @@
+from accounts.forms import AccountUpdateForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
+
 
 
 def hello_world(request):
@@ -22,3 +24,10 @@ class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accounts/detail.html'
+
+
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = AccountUpdateForm
+    success_url = reverse_lazy('accounts:hello_world')
+    template_name = 'accounts/update.html'
