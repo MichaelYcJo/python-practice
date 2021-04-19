d7094d49056b3996a0a83ee561718c28f2916901
Youngchan <michael_cho77@naver.com>
2021-04-19 20:37:42 +0900

    Add - Order Model

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)&-r+ft20sdw$=vq#v(61ik-_qyzd$sr50*@cc9(z)wz*c4zaz'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'shop',
     'cart',
     'coupon',
+    'order',
 ]
 
 MIDDLEWARE = [
@@ -152,3 +153,6 @@ LOGIN_REDIRECT_URL = '/'
 
 
 CART_ID = 'cart_items'
+
+IAMPORT_KEY = os.environ['IAMPORT_KEY']
+IAMPORT_SECRET_KEY=os.environ['IAMPORT_SECRET_KEY']
