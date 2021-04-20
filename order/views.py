76fd411f289efa62e08ec7265aa0a946c48c00b3
Youngchan <michael_cho77@naver.com>
2021-04-20 20:08:04 +0900

    ADD - CheckOut (-ing)

--- order/views.py
+++ order/views.py
@@ -26,9 +26,101 @@ def order_create(request):
 
 
 # JS 동작하지 않는 환경에서도 주문은 가능해야한다.
-# JS가 동작하지 않을 경우에만 실행되도록 추후 매팽할 예정
+# JS가 동작하지 않을 경우에만 실행되도록 매핑할 예정
 def order_complete(request):
     order_id = request.GET.get('order_id')
     #order = Order.objects.get(id=order_id)
     order = get_object_or_404(Order, id=order_id)
     return render(request, 'order/created.html', {'order':order})
+
+
+from django.views.generic.base import View
+from django.http import JsonResponse
+
+class OrderCreateAjaxView(View):
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return JsonResponse({"authenticated":False}, status=403)
+
+        cart = Cart(request)
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            # commit=False는 db에 저장하진 않는다. 
+            order = form.save(commit=False)
+            if cart.coupon:
+                order.coupon = cart.coupon
+                order.discount = cart.get_discount_total()
+            order.save()
+            for item in cart:
+                OrderItem.objects.create(order=order, product=item['product'], price=item['price'],
+                                         quantity=item['quantity'])
+            cart.clear()
+            data = {
+                "order_id":order.id
+            }
+            return JsonResponse(data)
+        else:
+            return JsonResponse({}, status=401)
+
+
+class OrderCheckoutAjaxView(View):
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return JsonResponse({"authenticated": False}, status=403)
+
+        order_id = request.POST.get('order_id')
+        order = Order.objects.get(id=order_id)
+        amount = request.POST.get('amount')
+
+        try:
+            merchant_order_id = OrderTransaction.objects.create_new(
+                order=order,
+                amount=amount
+            )
+        except:
+            merchant_order_id = None
+
+        if merchant_order_id is not None:
+            data = {
+                "works":True,
+                "merchant_id":merchant_order_id
+            }
+            return JsonResponse(data)
+        else:
+            return JsonResponse({}, status=401)
+
+
+class OrderImpAjaxView(View):
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return JsonResponse({"authenticated": False}, status=403)
+
+        order_id = request.POST.get('order_id')
+        order = Order.objects.get(id=order_id)
+
+        merchant_id = request.POST.get('merchant_id')
+        imp_id = request.POST.get('imp_id')
+        amount = request.POST.get('amount')
+
+        try:
+            trans = OrderTransaction.objects.get(
+                order=order,
+                merchant_order_id=merchant_id,
+                amount=amount
+            )
+        except:
+            trans = None
+
+        if trans is not None:
+            trans.transaction_id = imp_id
+            #trans.success = True
+            trans.save()
+            order.paid = True
+            order.save()
+
+            data = {
+                "works":True
+            }
+            return JsonResponse(data)
+        else:
+            return JsonResponse({}, status=401)
