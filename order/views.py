0c00bfc99133f1566a0620f1c21fe22b38a101b4
Youngchan <michael_cho77@naver.com>
2021-04-20 13:46:57 +0900

    ADD - Order Create

--- order/views.py
+++ order/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from cart.cart import Cart
+from django.shortcuts import render, get_object_or_404
+from order.models import *
+from order.forms import *
 
-# Create your views here.
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            if cart.coupon:
+                order.coupon = cart.coupon
+                # order.discount = cart.coupon.amount
+                # 할인에 대한 계산을 한 후에 그 금액값을 가져옴
+                order.discount = cart.get_discount_total()
+                order.save()
+            for item in cart:
+                OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
+            cart.clear()
+            return render(request, 'order/created.html', {'order':order})
+    else:
+        form = OrderCreateForm()
+    return render(request, 'order/create.html', {'cart':cart, 'form':form})
