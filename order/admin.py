2d95e10712bf681f57de284cb508fa4917181eff
Youngchan <michael_cho77@naver.com>
2021-04-21 23:25:15 +0900

    ADD - Order Base Admin

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,12 @@
+import datetime
 from django.contrib import admin
+from .models import Order, OrderItem
+from django.http import HttpResponse
 
-# Register your models here.
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id','first_name','last_name', 'email','address','postal_code','city','paid', 'created','updated']
+    list_filter = ['paid','created','updated']
+    inlines = []
+    actions = []
+
+admin.site.register(Order, OrderAdmin)
