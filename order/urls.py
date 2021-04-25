0f3a431ff69b20b52e367281ed319f2a9e635aba
Youngchan <michael_cho77@naver.com>
2021-04-25 13:33:12 +0900

    ADD - PDF Css

--- order/urls.py
+++ order/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('validation/', OrderImpAjaxView.as_view(), name='order_validation'),
     path('complete/', order_complete, name='order_complete'),
     path('order_detail/<int:order_id>/', admin_order_detail,name='admin_order_detail'),
+    path('order_pdf/<int:order_id>/', admin_order_pdf,name='admin_order_pdf'),
 ]
