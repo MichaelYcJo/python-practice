61cd3c0532744f65158db1430ea93c10252d3a08
Youngchan <michael_cho77@naver.com>
2021-05-08 23:09:08 +0900

    ADD - Variations Model(m2m)

--- carts/models.py
+++ carts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 from accounts.models import Account
-from store.models import Product
+from store.models import Product, Variation
 
 
 class Cart(models.Model):
@@ -14,6 +14,7 @@ class Cart(models.Model):
 
 class CartItem(models.Model):
     user = models.ForeignKey(Account, on_delete=models.CASCADE, null=True)
+    variations = models.ManyToManyField(Variation, blank=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     cart    = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True)
     quantity = models.IntegerField()
