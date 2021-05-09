2432226b0e69f1d172731e22525d09f6aa4701ab
Youngchan <michael_cho77@naver.com>
2021-05-09 22:28:43 +0900

    ADD - Variations Grouping

--- carts/models.py
+++ carts/models.py
@@ -18,7 +18,7 @@ class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     cart    = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True)
     quantity = models.IntegerField()
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True) 
 
     def sub_total(self):
         return self.product.price * self.quantity
