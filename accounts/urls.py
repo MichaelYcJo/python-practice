a9a5d83bc52f2e200f112ec5429cc31e909e43ce
Youngchan <michael_cho77@naver.com>
2021-05-16 17:43:06 +0900

    ADD - ResetPassword

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from accounts.views import register, login, logout, activate, dashboard
+from accounts.views import register, login, logout, activate, dashboard, forgotPassword, resetpassword_validate, resetPassword
 
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login, name='login'),
     path('logout/', logout, name='logout'),
     path('dashboard/', dashboard, name='dashboard'),
-    path('activate/<uidb64>/<token>/', activate, name='activate')
+    path('activate/<uidb64>/<token>/', activate, name='activate'),
+    path('forgotPassword/', forgotPassword, name='forgotPassword'),
+    path('resetpassword_validate/<uidb64>/<token>/', resetpassword_validate, name='resetpassword_validate'),
+    path('resetPassword/', resetPassword, name='resetPassword'),
 
 ]
