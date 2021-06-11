685125834f93e36bad6c1ac9ad6bc487b119a420
Youngchan <michael_cho77@naver.com>
2021-06-11 23:22:06 +0900

    ADD - Rating Component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 export default function Product({ product }) {
     return (
@@ -18,6 +19,7 @@ export default function Product({ product }) {
                 <Card.Text as="div">
                     <div className="my-3">
                         {product.rating} from {product.numReviews} reviews
+                        <Rating value={product.rating} text={`${product.numReviews} reviews`} color={`#f82825`} />
                     </div>
                 </Card.Text>
 
