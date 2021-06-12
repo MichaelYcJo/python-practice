448e9c5a12ba3fd1c4ec3a13fe5a6aa6a162d438
Youngchan <michael_cho77@naver.com>
2021-06-13 02:03:48 +0900

    ADD - Backend Setting, api_view

---
+++ backend/base/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from base.views import getRoutes, getProducts, getProduct
+
+
+
+urlpatterns = [
+    path('', getRoutes, name="routes"),
+    path('products/', getProducts, name="products"),
+    path('product/<str:pk>/', getProduct, name="product"),
+]
