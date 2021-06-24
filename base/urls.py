80af508de8776571b7b2ad4b62e7e214c20e220a
Youngchan <michael_cho77@naver.com>
2021-06-25 00:29:03 +0900

    ADD - JWT Customizing

--- base/urls.py
+++ base/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
-from base.views import getRoutes, getProducts, getProduct
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-)
+from base.views import getRoutes, getProducts, getProduct, MyTokenObtainPairView
+
 
 
 
 urlpatterns = [
-    path('api/users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/users/login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', getRoutes, name="routes"),
     path('products/', getProducts, name="products"),
     path('products/<str:pk>/', getProduct, name="product"),
--- base/views.py
+++ base/views.py
@@ -3,10 +3,29 @@ from django.http import JsonResponse
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from base.products import products 
 from base.models import Product
 from base.serializers import ProductSerializer
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        token['message'] = 'auth test'
+        # ...
+
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
 
 
 @api_view(['GET'])
