3cb6e2cd4fa1ecf77ff5b5f4e91853445c2a4aa5
Youngchan <michael_cho77@naver.com>
2021-06-24 20:55:32 +0900

    ADD - SimpleJWT Setup

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from base.views import getRoutes, getProducts, getProduct
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
 
 
 
 urlpatterns = [
+    path('api/users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', getRoutes, name="routes"),
     path('products/', getProducts, name="products"),
     path('products/<str:pk>/', getProduct, name="product"),
 ]
+
+
