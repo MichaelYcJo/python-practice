07c82ff6917fd8c1a517762bd31a4c97c1972df1
Youngchan <michael_cho77@naver.com>
2021-07-05 22:45:04 +0900

    ADD - UpdateProfile in DRF

--- base/urls/user_urls.py
+++ base/urls/user_urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from base.views.user_views import MyTokenObtainPairView, registerUser, getuserProfile, getUsers
+from base.views.user_views import MyTokenObtainPairView, registerUser, getuserProfile, getUsers, updateUserProfile
  
 
 
@@ -8,6 +8,7 @@ urlpatterns = [
     path('register/', registerUser, name='register'),
     path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('profile/', getuserProfile, name="user-profile"),
+    path('profile/update/', updateUserProfile, name='user-profile-update'),
     path('', getUsers, name="users"),
 ]
 
