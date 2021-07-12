a92dfa2ecc99200d64a9921429b0f8212832cde9
Youngchan <michael_cho77@naver.com>
2021-07-12 23:37:14 +0900

    feature/Get Order By Id Endpoint

--- base/urls/order_urls.py
+++ base/urls/order_urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-
+from base.views.order_views import getOrders, addOrderItems
 
 urlpatterns = [
+    path('', getOrders, name='orders'),
+    path('add/', addOrderItems, name='orders-add'),
 
 ]
 
