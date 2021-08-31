e51e15804b3c44fe4c710485768a7be83b084af8
Youngchan <michael_cho77@naver.com>
2021-08-31 20:35:18 +0900

    feature/Cookie, Header

--- fastapi-base/main.py
+++ fastapi-base/main.py
@@ -1,28 +1,13 @@
 import uvicorn
 
-from typing import List
-
-from fastapi import FastAPI
-from pydantic import BaseModel, Field
+from fastapi import FastAPI, Header
 
 app = FastAPI()
 
-class Item(BaseModel):
-    name: str = Field(..., min_length=1, max_length=100, title="이름")
-    price: float = Field(None, ge=0)
-    amount: int = Field(
-        default=1,
-        gt=0,
-        le=100,
-        title="수량",
-        description="아이템 갯수. 1~100 개 까지 소지 가능",
-    )
 
-
-@app.post("/users/{user_id}/item")
-def create_item(item: Item):
-    return item
-
+@app.get("/header")
+def get_headers(x_token: str = Header(None, title="토큰")):
+    return {"X-Token": x_token}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
