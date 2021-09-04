6283d5d4ca2812d3aaf59ffc682acc071675acba
Youngchan <michael_cho77@naver.com>
2021-09-05 00:43:39 +0900

    feature/Nav, GlobalStyle

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { GlobalStyle } from './globalStyles';
+import Navbar from "components/Navbar";
 
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <GlobalStyle />
+      <Navbar />
+    </Router>
   );
 }
 
