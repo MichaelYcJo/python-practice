4a15e3fd36692a224188066dc7e04206ba9cd7d5
Youngchan <michael_cho77@naver.com>
2021-09-05 19:25:27 +0900

    feature/SideBar

--- frontend/src/components/Main/index.js
+++ frontend/src/components/Main/index.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from 'components/Navbar'
 import { MainContainer, MainContent, MainItems, MainH1, MainP, MainBtn } from 'components/Main/MainElements'
+import Sidebar from 'components/Sidebar'
 
 
 export default function Main() {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => {
+        setIsOpen(!isOpen);
+    }
     return (
         <MainContainer>
-            <Navbar />
+            <Navbar toggle={toggle} />
+            <Sidebar isOpen={isOpen} toggle={toggle} />
             <MainContent>
                 <MainItems>
                     <MainH1>Attractive Ever</MainH1>
---
+++ frontend/src/components/Sidebar/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+    SidebarContainer,
+    Icon,
+    CloseIcon,
+    SidebarMenu,
+    SidebarLink,
+    SidebarRoute,
+    SideBtnWrap
+} from 'components/Sidebar/SidebarElements';
+
+const Sidebar = ({ isOpen, toggle }) => {
+    return (
+        <SidebarContainer isOpen={isOpen} onClick={toggle}>
+            <Icon onClick={toggle}>
+                <CloseIcon />
+            </Icon>
+            <SidebarMenu>
+                <SidebarLink to='/'>New Releases</SidebarLink>
+                <SidebarLink to='/'>Men</SidebarLink>
+                <SidebarLink to='/'>Women</SidebarLink>
+            </SidebarMenu>
+            <SideBtnWrap>
+                <SidebarRoute to='/'>Order Now</SidebarRoute>
+            </SideBtnWrap>
+        </SidebarContainer>
+    );
+};
+
+export default Sidebar;
