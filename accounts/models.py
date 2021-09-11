4fedc41e61a38917dbd4eb2f07adf4d630bcde2d
Youngchan <michael_cho77@naver.com>
2021-09-11 23:54:58 +0900

    feature/User Model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=200, unique=True)
+    email_checked = models.BooleanField(default=False)
+    phone = models.CharField(max_length=100, blank=True, null=True)
+    special_code = models.CharField(max_length=10, blank=True, null=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False)
+
+
+    USERNAME_FIELD = "email"
+
+
+    def __str__(self):
+        return str(f'{self.first_name} {self.last_name}')
+
