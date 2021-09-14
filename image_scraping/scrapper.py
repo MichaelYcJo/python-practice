64e4ac426ff353730e044791bf55c5ff4b9b1c22
Youngchan <michael_cho77@naver.com>
2021-09-14 18:50:48 +0900

    feature/Apply Logic

--- image_scraping/scrapper.py
+++ image_scraping/scrapper.py
@@ -40,22 +40,43 @@ for i in range(1, 57):
             "image_name" : i.get('src').split('/')[-1]
         })
 
- 
+  
         full_src = src + 'v1/' + quote_plus(image_name)
         print('전체 URL', full_src)
         urlretrieve(full_src, './images/' + image_name)
-
-
+     
 
 for i, j in zip(href_data, src_data):
     json_data.append({
         "clay_ful_id" : i['clay_ful_id'],
-        "image_path" : j['image_name']
+        "image_path" : f'product/thumbnail/{j["image_name"]}'
     })
 
 with open(path, 'w',  encoding='UTF-8-sig') as f:
     json.dump(json_data, f, ensure_ascii = False, indent=4)
         
+
+'''
+적용로직
+    from store.models import StoreProduct
+    import os
+    from django.conf import settings
+    info = None
+    with open(os.path.join(settings.BASE_DIR, 'info.json'), 'r', encoding="utf-8-sig") as f:
+        info = json.load(f)
+    u_product_list = StoreProduct.objects.all()
+
+    for i in info:
+        try:
+            custom_product = StoreProduct.objects.get(clayful_product_id=i['clay_ful_id'])
+            print(custom_product)
+            custom_product.product_image = i['image_path']
+            custom_product.save()
+            print(custom_product.model_name + ' 성공')
+        except StoreProduct.DoesNotExist:
+            print(i['clay_ful_id'] + ' 없음')
+
+'''
 
 
 '''
