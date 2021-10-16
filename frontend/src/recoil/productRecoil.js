c1321d9fb65af08c6378756e8e44a9c643be4394
Youngchan <michael_cho77@naver.com>
2021-10-16 13:52:42 +0900

    feature/ALL Template Apply

--- frontend/src/recoil/productRecoil.js
+++
@@ -1,8 +0,0 @@
-import { atom} from 'recoil';
-
-
-export const productState = atom({
-    key: 'product',
-    default: []
-
-});
--- frontend/src/screens/UserProfile/UserProfilePresenter.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function UserProfilePresenter() {
-    return (
-        <div>
-            UserProfile
-        </div>
-    )
-}
