88d51606dcb274c7a4e6cb05867b3231aa5bbc42
Youngchan <michael_cho77@naver.com>
2021-11-14 19:28:13 +0900

    feature/Divied Setting

--- config/settings/base.py
+++ config/settings/base.py
@@ -15,25 +15,18 @@ import os
 from pathlib import Path
 from datetime import timedelta
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8rf*a=+a2oc3&szek873d7^a57z9(x$dliky(@h=2fvs47uw!v'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
-
+DEBUG = config('DEBUG', False)
 
 # Application definition
-
-
 DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -90,17 +83,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -139,7 +121,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
     BASE_DIR / 'frontend/build/static',
     BASE_DIR / 'frontend/public/assets'
 ]
---
+++ config/settings/deploy.py
@@ -0,0 +1,32 @@
+from .base import *
+
+
+# Docker Secrets 파일을 읽어서 env로 활용
+def read_secret(secret_name):
+    file = open('/run/secrets/' + secret_name)
+    secret = file.read()
+    secret = secret.rstrip().lstrip()
+    file.close()
+    return secret
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+
+# SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
+
+ALLOWED_HOSTS = ['*']
+
+
+# Database
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": 'michael_shop',
+        "USER": 'michael',
+        "PASSWORD": read_secret('POSTGRES_PASSWORD'),
+        "HOST": '158.247.224.15',
+        "PORT": 5432,
+    }
+}
