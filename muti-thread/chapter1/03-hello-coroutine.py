6e1e931f94e0b473f4e85b0e2421f37e42e1c0a1
Youngchan <michael_cho77@naver.com>
2021-11-23 21:31:54 +0900

    Python 코루틴 개념 이해

---
+++ muti-thread/chapter1/03-hello-coroutine.py
@@ -0,0 +1,12 @@
+# https://docs.python.org/ko/3/library/asyncio-task.html
+
+import asyncio
+
+
+async def hello_world():
+    await print("hello world")
+    return 123
+
+
+if __name__ == "__main__":
+    asyncio.run(hello_world())
