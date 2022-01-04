f314217d59b5f1b100105e07022c49cc4987d6cc
Youngchan <michael.yc.cho@gmail.com>
2022-01-04 23:54:21 +0900

    feature/Django TestCase(Unit Test)

--- pytest-with-drf/classroom/tests.py
+++ pytest-with-drf/classroom/tests.py
@@ -1,3 +1,60 @@
 from django.test import TestCase
+from classroom.models import Student
 
-# Create your tests here.
+
+class TestStudentModel(TestCase):
+    def test_add_a_plus_b(self):
+        a = 1
+        b = 2
+        c = a + b
+
+        self.assertEqual(c, 3)
+
+    def test_student_can_be_created(self):
+
+        student1 = Student.objects.create(
+            first_name="John", last_name="Doe", admission_number=1234
+        )
+
+        student_result = Student.objects.last()  # getting the last student
+
+        self.assertEqual(student_result.first_name, "John")
+
+    def test_str_return(self):
+        student1 = Student.objects.create(
+            first_name="John", last_name="Doe", admission_number=1234
+        )
+
+        student_result = Student.objects.last()  # getting the last student
+
+        self.assertEqual(str(student_result), "John")
+
+    def test_grade_fail(self):
+
+        student1 = Student.objects.create(
+            first_name="John", last_name="Doe", admission_number=1234, average_score=10
+        )
+
+        student_result = Student.objects.last()  # getting the last student
+
+        self.assertEqual(student_result.get_grade(), "Fail")
+
+    def test_grade_pass(self):
+
+        student1 = Student.objects.create(
+            first_name="John", last_name="Doe", admission_number=1234, average_score=60
+        )
+
+        student_result = Student.objects.last()  # getting the last student
+
+        self.assertEqual(student_result.get_grade(), "Pass")
+
+    def test_grade_excellent(self):
+
+        student1 = Student.objects.create(
+            first_name="John", last_name="Doe", admission_number=1234, average_score=90
+        )
+
+        student_result = Student.objects.last()  # getting the last student
+
+        self.assertEqual(student_result.get_grade(), "Excellent")
