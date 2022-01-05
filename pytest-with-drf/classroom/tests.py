379826f0570a0fd21fbb53af99187b473c1a5c08
Youngchan <michael.yc.cho@gmail.com>
2022-01-05 23:51:51 +0900

    feature/SetUp()

--- pytest-with-drf/classroom/tests.py
+++ pytest-with-drf/classroom/tests.py
@@ -3,40 +3,26 @@ from classroom.models import Student
 
 
 class TestStudentModel(TestCase):
-    def test_add_a_plus_b(self):
-        a = 1
-        b = 2
-        c = a + b
 
-        self.assertEqual(c, 3)
+    def setUp(self):
+            self.student1 = Student.objects.create(
+            first_name="John", last_name="Doe", admission_number=12345
+        )
 
     def test_student_can_be_created(self):
-
-        student1 = Student.objects.create(
-            first_name="John", last_name="Doe", admission_number=1234
-        )
-
-        student_result = Student.objects.last()  # getting the last student
-
-        self.assertEqual(student_result.first_name, "John")
+        self.assertEqual(self.student1.first_name, "John")
 
     def test_str_return(self):
-        student1 = Student.objects.create(
-            first_name="John", last_name="Doe", admission_number=1234
-        )
-
-        student_result = Student.objects.last()  # getting the last student
-
-        self.assertEqual(str(student_result), "John")
+        self.assertEqual(str(self.student1), "John")
 
     def test_grade_fail(self):
-
+        
         student1 = Student.objects.create(
-            first_name="John", last_name="Doe", admission_number=1234, average_score=10
+            first_name="John", last_name="Doe", admission_number=1234, average_score=30
         )
 
         student_result = Student.objects.last()  # getting the last student
-
+        
         self.assertEqual(student_result.get_grade(), "Fail")
 
     def test_grade_pass(self):
@@ -58,3 +44,11 @@ class TestStudentModel(TestCase):
         student_result = Student.objects.last()  # getting the last student
 
         self.assertEqual(student_result.get_grade(), "Excellent")
+
+
+    def test_add_a_plus_b(self):
+        a = 1
+        b = 2
+        c = a + b
+
+        self.assertEqual(c, 3)
