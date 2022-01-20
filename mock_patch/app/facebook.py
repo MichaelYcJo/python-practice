117a5955254e45357b4491f8ffcc2798f671540a
Youngchan <michael.yc.cho@gmail.com>
2022-01-20 23:00:18 +0900

    feature/UnitTest Mock

---
+++ mock_patch/app/facebook.py
@@ -0,0 +1,5 @@
+from app.data import get_data
+
+def call_facebook_api():
+    data = get_data()
+    return data
---
+++ mock_patch/app/google.py
@@ -0,0 +1,5 @@
+from app.data import get_data
+
+def call_google_api():
+    data = get_data()
+    return data
