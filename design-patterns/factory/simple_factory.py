24ae6ed0836c937e4e60821e0cc790df7cecce87
Youngchan <michael.yc.cho@gmail.com>
2022-01-23 01:58:59 +0900

    feature/Comment

--- design-patterns/factory/simple_factory.py
+++ design-patterns/factory/simple_factory.py
@@ -1,6 +1,13 @@
 from abc import ABCMeta, abstractclassmethod
 
+'''
+심플 팩토리 패턴
+- 인터페이스를 통해 객체를 생성하지만 서브 클래스가 객체생성에 필요한 클래스를 선택한다.
+- 팩토리 메소드와 추상팩토리 메소드 패턴을 이해하기위한 기본개념정도
+- 여러 종류의 객체를 사용자가 직접 클래스를 호출하지 않고도 생성할 수 있다.
+'''
 
+# ABCMeta는 파이썬에서 특정 클래스를 abstrac으로 선언하는 특수 메타클래스이다.
 class Animal(metaclass = ABCMeta):
     @abstractclassmethod
     def do_say(self):
