9732cb1d8d61a3424799ac7763ae836e7dedb527
Youngchan <michael.yc.cho@gmail.com>
2022-02-08 21:19:32 +0900

    feature/Setting, url파라미터

---
+++ fast_api_facam/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/users/me")
+def get_current_user():
+    return {"user_id": 123}
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    return {"user_id": user_id}
