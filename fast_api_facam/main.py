6fb4d860cb7a838de53eb85f3bcda782f070fd10
Youngchan <michael.yc.cho@gmail.com>
2022-02-09 22:45:59 +0900

    feature/Request, 중첩모델

--- fast_api_facam/main.py
+++ fast_api_facam/main.py
@@ -1,15 +1,40 @@
-from enum import Enum
+from typing import Optional, List  # 추가: List
 
 from fastapi import FastAPI
+from pydantic import BaseModel, HttpUrl
+
 
 app = FastAPI()
 
-class UserLevel(str, Enum):
-    a = "a"
-    b = "b"
-    c = "c"
+
+# 추가: Item 클래스
+class Item(BaseModel):
+    name: str
+    price: float
+    amount: int = 0
 
 
-@app.get("/users")
-def get_users(grade: UserLevel):
-    return {"grade": grade}
+class User(BaseModel):
+    name: str
+    password: str
+    avatar_url: Optional[HttpUrl] = None
+    inventory: List[Item] = []  # 추가: inventory
+
+
+@app.post("/users")
+def create_user(user: User):
+    return user
+
+
+# 추가: get_user()
+@app.get("/users/me")
+def get_user():
+    fake_user = User(
+        name="FastCampus",
+        password="1234",
+        inventory=[
+            Item(name="전설 무기", price=1_000_000),
+            Item(name="전설 방어구", price=900_000),
+        ]
+    )
+    return fake_user
