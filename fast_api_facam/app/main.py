a9b1e401a1c4086ffabd582ce4b009b4683b2394
Youngchan <michael.yc.cho@gmail.com>
2022-02-12 22:19:19 +0900

    Comment

--- fast_api_facam/app/main.py
+++ fast_api_facam/app/main.py
@@ -6,15 +6,18 @@ from sqlalchemy.orm import Session
 from . import models, schemas
 from .database import SessionLocal, engine
 
+# DB에서 models에서 정의한 테이블(User)를 생성하겠다는 의미
+# 앱실행시 테이블이 자동으로 만들어진다.
 models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
 
-
+# DB를 사용하기 위한 일종의 helper 함수
 def get_db():
     db = SessionLocal()
     try:
+        # return은 반환하고 함수가 끝나지만, yield는 반환하고 함수가 끝나지 않는다
         yield db
     finally:
         db.close()
