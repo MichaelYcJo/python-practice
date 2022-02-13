d6011315907c191b4c72577850c3e518944da3f7
Youngchan <michael.yc.cho@gmail.com>
2022-02-13 23:17:26 +0900

    feature/Form

--- fast_api_facam/app/main.py
+++ fast_api_facam/app/main.py
@@ -1,6 +1,8 @@
 from typing import List
 
-from fastapi import Depends, FastAPI, HTTPException
+
+from fastapi import Depends, FastAPI, Form, HTTPException
+from fastapi.staticfiles import StaticFiles
 from sqlalchemy.orm import Session
 
 from . import models, schemas
@@ -12,6 +14,8 @@ models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
+# mount : static파일을 import하게해줌. 주소는 /static static에 디렉토리를 잡아주면된다.
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # DB를 사용하기 위한 일종의 helper 함수
 def get_db():
@@ -42,3 +46,8 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 @app.get("/users", response_model=List[schemas.User])
 def read_users(db: Session = Depends(get_db)):
     return db.query(models.User).all()
+
+
+@app.post("/login")
+def login(username: str = Form(...), password: str = Form(...)):
+    return {"username": username}
