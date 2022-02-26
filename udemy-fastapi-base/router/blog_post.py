84f3aafd9ea7c731284909b070408da0b91b5da1
Youngchan <michael.yc.cho@gmail.com>
2022-02-26 23:19:19 +0900

    feature/MetaData

--- udemy-fastapi-base/router/blog_post.py
+++ udemy-fastapi-base/router/blog_post.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import APIRouter
+from fastapi import APIRouter, Body, Query
 from pydantic import BaseModel
 
 router = APIRouter(
@@ -19,4 +19,23 @@ def create_blog(blog: BlogModel, id: int, version: int = 1):
         'id': id,
         'data': blog,
         'version': version
+        }
+    
+@router.post('/new/{id}/comment')
+def create_comment(blog: BlogModel, id: int,
+                    comment_id: int = Query(None,
+                    title="ID of the Comment",
+                    description='Some description for comment id',
+                    alias='commentID'
+                    ),
+                    content: str = Body(...,
+                                        min_length=10,
+                                        max_length=50,
+                                        regex='^[a-z\s]*$')
+                ):
+    return {
+        'id': id,
+        'data': blog,
+        'comment_id': comment_id,
+        'content': content
         }
