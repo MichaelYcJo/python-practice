9258af472266326c3002578fa72dea0f6ff6aec2
Youngchan <michael.yc.cho@gmail.com>
2022-02-28 23:19:19 +0900

    feature/Number Validator

--- udemy-fastapi-base/router/blog_post.py
+++ udemy-fastapi-base/router/blog_post.py
@@ -1,5 +1,5 @@
 from typing import List, Optional
-from fastapi import APIRouter, Body, Query
+from fastapi import APIRouter, Body, Path, Query
 from pydantic import BaseModel
 
 router = APIRouter(
@@ -21,24 +21,26 @@ def create_blog(blog: BlogModel, id: int, version: int = 1):
         'version': version
         }
     
-@router.post('/new/{id}/comment')
+@router.post('/new/{id}/comment/{comment_id}')
 def create_comment(blog: BlogModel, id: int,
-                    comment_id: int = Query(None,
-                    title="ID of the Comment",
-                    description='Some description for comment id',
-                    alias='commentID'
+                    comment_title: int = Query(None,
+                    title="Title of the Comment",
+                    description='Some description for comment Title',
+                    alias='commentTitle'
                     ),
                     content: str = Body(...,
                                         min_length=10,
                                         max_length=50,
                                         regex='^[a-z\s]*$'
                                         ),
-                    v: Optional[List[str]] = Query(None)
+                    v: Optional[List[str]] = Query(None),
+                    comment_id: int = Path(None, gt=5, le=10)
                 ):
     return {
         'id': id,
         'data': blog,
-        'comment_id': comment_id,
+        'comment_title': comment_title,
         'content': content,
-        'versions':v
+        'versions':v,
+        'comment_id': comment_id,
         }
