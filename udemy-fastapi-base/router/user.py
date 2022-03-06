199696b002e6dd726a6fe1049000331068b8611b
Youngchan <michael.yc.cho@gmail.com>
2022-03-06 22:49:45 +0900

    feature/Read Database

--- udemy-fastapi-base/router/user.py
+++ udemy-fastapi-base/router/user.py
@@ -1,6 +1,7 @@
 from schemas import UserBase, UserDisplay
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from typing import List
 from db.database import get_db
 from db import db_user
 
@@ -15,7 +16,15 @@ router = APIRouter(
 def create_user(request: UserBase, db: Session = Depends(get_db)):
   return db_user.create_user(db, request)
 
+# Read Akk Ysers
+@router.get('/', response_model=List[UserDisplay])
+def get_all_users(db: Session = Depends(get_db)):
+  return db_user.get_all_users(db)
+
 # Read user
+@router.get('/{id}', response_model=UserDisplay)
+def get_user(id: int, db: Session = Depends(get_db)):
+  return db_user.get_user(db, id)
 
 # Update user
 
