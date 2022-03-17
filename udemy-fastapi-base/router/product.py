6d53a1f55b880c1281ac05d97b5ab8e2f7aee684
Youngchan <michael.yc.cho@gmail.com>
2022-03-17 22:31:48 +0900

    feature/Async ~ Await

--- udemy-fastapi-base/router/product.py
+++ udemy-fastapi-base/router/product.py
@@ -12,6 +12,10 @@ router = APIRouter(
 products = ['watch', 'camera', 'phone']
 
 
+async def time_consuming_functionality():
+  time.sleep(5)
+  return 'ok'
+
 @router.post('/new')
 def create_product(name: str = Form(...)):
   products.append(name)
@@ -19,8 +23,9 @@ def create_product(name: str = Form(...)):
 
 
 @router.get('/all')
-def get_all_products():
+async def get_all_products():
   log("API", "Call Get All Products")
+  await time_consuming_functionality()
   # return products
   data = " ".join(products)
   response = Response(content=data, media_type="text/plain")
