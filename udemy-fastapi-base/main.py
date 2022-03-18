4f110972eb3b4ca11178fffe3850c7a84cf75223
Youngchan <michael.yc.cho@gmail.com>
2022-03-18 23:15:39 +0900

    feature/Template

--- udemy-fastapi-base/main.py
+++ udemy-fastapi-base/main.py
@@ -1,44 +1,48 @@
-from fastapi import FastAPI, HTTPException, Request
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse, PlainTextResponse
-from fastapi.staticfiles import StaticFiles
+from os import name
+from fastapi.exceptions import HTTPException
+from fastapi.responses import PlainTextResponse
 from exceptions import StoryException
-from router import article, blog_get, blog_post, user, product, file
+from fastapi import FastAPI
+from router import blog_get, blog_post, user, article, product, file
 from auth import authentication
+from templates import templates
 from db import models
 from db.database import engine
+from fastapi import Request
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-app.include_router(file.router)
+app.include_router(templates.router)
 app.include_router(authentication.router)
-app.include_router(article.router)
+app.include_router(file.router)
 app.include_router(user.router)
+app.include_router(article.router)
+app.include_router(product.router)
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
-app.include_router(product.router)
 
 @app.get('/hello')
 def index():
-  return {'message': 'Hello world!'}
+    return {'message': 'Hello world!'}
 
 @app.exception_handler(StoryException)
 def story_exception_handler(request: Request, exc: StoryException):
   return JSONResponse(
-    status_code=410,
+    status_code=418,
     content={'detail': exc.name}
   )
 
+# @app.exception_handler(HTTPException)
+# def custom_handler(request: Request, exc: StoryException):
+#   return PlainTextResponse(str(exc), status_code=400)
 
-@app.exception_handler(HTTPException)
-def custom_handler(request: Request, exc: StoryException):
-  return PlainTextResponse(str(exc), status_code=400)
-  
-
-models.Base.metadata.create_all(engine)\
+models.Base.metadata.create_all(engine)
 
 origins = [
-           'http://localhost:3000',
-           ]
+  'http://localhost:3000'
+]
 
 app.add_middleware(
   CORSMiddleware,
@@ -48,5 +52,8 @@ app.add_middleware(
   allow_headers = ['*']
 )
 
-
-app.mount('/files', StaticFiles(directory='files'), name='files')
+app.mount('/files', StaticFiles(directory="files"), name='files')
+app.mount('/templates/static', 
+      StaticFiles(directory="templates/static"),
+      name="static"
+)
