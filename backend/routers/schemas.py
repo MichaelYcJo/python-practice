707581aefd7f2884409c6a6bbff8ccfc8fba31e2
Youngchan <michael.yc.cho@gmail.com>
2022-04-10 23:16:02 +0900

    add: CreatePost

--- backend/routers/schemas.py
+++ backend/routers/schemas.py
@@ -9,9 +9,31 @@ class UserBase(BaseModel):
   email: str
   password: str
   
+class PostBase(BaseModel):
+  image_url: str
+  image_url_type: str
+  caption: str
+  creator_id: int
   
 class UserDisplay(BaseModel):
   username: str
   email: str
+  class Config():
+    orm_mode = True
+  
+  
+# For PostDisplay
+class User(BaseModel):
+  username: str
+  class Config():
+    orm_mode = True
+  
+class PostDisplay(BaseModel):
+  id: int
+  image_url: str
+  image_url_type: str
+  caption: str
+  timestamp: datetime
+  user: User
   class Config():
     orm_mode = True
