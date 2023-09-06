2c40a2f78178aa6838d4a33a6aeb9c4ba796c919
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-09-06 22:09:46 +0900

    chore: 파일정리

--- mock_patch/app/facebook.py
+++
@@ -1,5 +0,0 @@
-from app.data import get_data
-
-def call_facebook_api():
-    data = get_data()
-    return data
--- mock_patch/app/google.py
+++
@@ -1,5 +0,0 @@
-from app.data import get_data
-
-def call_google_api():
-    data = get_data()
-    return data
