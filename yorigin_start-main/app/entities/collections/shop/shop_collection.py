6fcbcdbab12b4e0038e995c1c18d01a7d3494dcb
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2024-09-19 22:52:32 +0900

    feat: Index Setting

--- yorigin_start-main/app/entities/collections/shop/shop_collection.py
+++ yorigin_start-main/app/entities/collections/shop/shop_collection.py
@@ -10,10 +10,27 @@ from app.entities.collections.shop.shop_document import (
     ShopDocument,
 )
 from app.utils.mongo import db
+import pymongo
 
 
 class ShopCollection:
     _collection = AsyncIOMotorCollection(db, "shops")
+
+    """
+    첫 번째 매개변수 ("delivery_areas.poly"):
+        •	이 부분은 인덱스를 설정할 필드 이름입니다.
+        •	즉, 여기서는 delivery_areas라는 필드의 하위 필드인 poly에 대해 인덱스를 생성하겠다는 의미입니다.
+        2.	두 번째 매개변수 (pymongo.GEOSPHERE):
+        •	이 부분은 인덱스의 타입을 나타냅니다.
+        •	pymongo.GEOSPHERE는 지리적 구면 좌표 인덱스를 뜻하며, MongoDB에서 제공하는 지리적 쿼리를 효율적으로 처리할 수 있도록 설정하는 특별한 인덱스 타입입니다.
+        •	이 타입은 **구 좌표계(지구 표면을 기준으로 하는 좌표)**를 기반으로 거리 계산을 하거나, 특정 범위 내에 있는 위치를 검색하는 데 사용됩니다.
+        
+        compass > explain plan > {'delivery_areas.poly': {$geoIntersects: { $geometry: { type: 'Point', coordinates: [ 126.9234, 37.611 ] }}}}
+    """
+
+    @classmethod
+    async def set_index(self) -> None:
+        await self._collection.create_index([("delivery_areas.poly", pymongo.GEOSPHERE)])
 
     @classmethod
     async def get_distinct_category_codes_by_point_intersects(cls, point: GeoJsonPoint) -> list[CategoryCode]:
--- yorigin_start-main/app/main.py
+++ yorigin_start-main/app/main.py
@@ -1,3 +1,11 @@
 from fastapi import FastAPI
 
+from app.entities.collections import set_indexes
+
 app = FastAPI()
+
+
+# fastapi 가 시작할 때마다 인댁스를 생성(app.on_event(), asgi.py를 실행하면 인덱스가 생성되는걸 볼 수있음)
+@app.on_event("startup")
+async def on_startup() -> None:
+    await set_indexes()
