684ccae5e13f6a76702976ccac6f9e150aa79eee
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2024-09-19 22:27:37 +0900

    feat: Mock Data env setting

--- yorigin_start-main/assets/insert_shops.py
+++ yorigin_start-main/assets/insert_shops.py
@@ -1,14 +1,23 @@
 import asyncio
 import json
 import os
+from pathlib import Path
 
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorCollection
 from pymongo.errors import BulkWriteError
 
-DATABASE_NAME = os.environ.get("MONGO_DATABASE", "yorigin")
+from dotenv import load_dotenv
 
-client = AsyncIOMotorClient()
-db = client[DATABASE_NAME]
+# .env 파일 로드
+env_path = Path(__file__).parent.parent / ".env"
+load_dotenv(dotenv_path=env_path)
+
+MONGO_DATABASE = os.getenv("MONGO_DATABASE")
+MONGO_URL = os.getenv("MONGO_URI")
+
+
+client = AsyncIOMotorClient(MONGO_URL)
+db = client[MONGO_DATABASE]
 shop_collection = AsyncIOMotorCollection(db, "shops")
 
 
